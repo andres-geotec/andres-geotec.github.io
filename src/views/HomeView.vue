a89dfde005ae5257703d5bcfba1cb4178cd1a2f3
andres-geotec <andres.geotec@gmail.com>
2025-01-06 22:03:41 -0600

    🔧 feat: integración de sisdai y maplibre-gl

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,35 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { Map } from 'maplibre-gl'
+import 'maplibre-gl/dist/maplibre-gl.css'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  new Map({
+    container: 'map',
+    style: 'https://demotiles.maplibre.org/style.json',
+    center: [0, 0],
+    zoom: 1,
+  })
+})
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="portada">
+    <div id="map" class="portada-imagen" />
+    <div class="portada-degradado">
+      <div class="portada-cuerpo">
+        <p class="portada-subtitulo">Hola, soy</p>
+        <h1 class="portada-titulo">andres-geotec</h1>
+      </div>
+    </div>
+  </div>
+  <!-- <main>:)</main> -->
+
+  <!-- <div id="map"></div> -->
 </template>
+
+<style lang="scss">
+#map {
+  height: 100%;
+}
+</style>
